6c856d13b4d8256634936bf6ccc5b89a7d683d00
BetterLordWilliam <willjordan05@icloud.com>
2025-06-13 13:29:49 -0700

    WO: Removed write-host commands used for testing

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,7 @@
 ## Custom Aliases
 Set-Alias ff fastfetch
 
-## Use EPIC oh-my-posh
-$POSH_CONFIG=($PSScriptRoot -replace "\\", "/")+"/poshconfigs/simpleexample.json"
-Write-Host $POSH_CONFIG
+## Oh-My-Posh Config
+$POSH_CONFIG=($PSScriptRoot -replace "\\", "/")+"/poshconfigs/simpleexample.json"   # Use unix paths for consistency
 oh-my-posh --config $POSH_CONFIG init pwsh | Invoke-Expression
 
