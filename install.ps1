a5c89687aa335ee5061098851e54055206a1237d
LordWilliam <willjordan05@icloud.com>
2026-04-03 23:49:27 -0700

    WO: yasb added to install command

--- install.ps1
+++ install.ps1
@@ -1,5 +1,6 @@
 winget install ScaryRawr.SL;            # massively important
 winget install JanDeDobbeleer.OhMyPosh; # command prompt stylings
+winget install AnM.yasb;                # status bar program
 
 Install-Module -Name VirtualDesktop
 
